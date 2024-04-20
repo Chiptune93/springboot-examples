217e2091ecb254a6236815fa5074d453043e1d41
Chiptune93 <eoen012@gmail.com>
2024-04-20 18:43:48 +0900

    :construction: 메인 샘플 추가
    (cherry picked from commit 4ae1ae08c4e95bf10dfaab6a7116d0f07906fdd5)

--- src/main/java/dev/chiptune/springboot/entity/TestUser.java
+++ src/main/java/dev/chiptune/springboot/entity/TestUser.java
@@ -1,9 +1,6 @@
 package dev.chiptune.springboot.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,6 +12,7 @@ import javax.persistence.Id;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class TestUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
